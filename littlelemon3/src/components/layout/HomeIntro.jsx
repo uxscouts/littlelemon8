872dab6a06f7590032fc3672d119c1eb06396719
UX SCOUTS <admin@uxscouts.com>
2026-02-27 16:35:29 +0000

    intro code from onecompiler

---
+++ littlelemon3/src/components/layout/HomeIntro.jsx
@@ -0,0 +1,27 @@
+
+function HomeIntro() {
+  return (
+    <>
+    <section class="big-green-section"> 
+        <div class="container-auto">
+          <div class="grid-item">
+            <h1 class="karla-400 lemon-yellow">LITTLE LEMON</h1>
+            <h2 class="karla-300 white">Chicago</h2>
+            <p class="karla-400 white">
+              we are a family owned Mediterranian restaurant, 
+              focused on traditional recipes with a modern twist.
+              </p>
+            <p>
+              <button type="button" class="btn btn-warning"> Reserve a Table </button>
+            </p>
+          </div>
+          <div class="grid-item lightGrey">
+            <div class="Tray_of_Fancy_Sushi"></div>
+          </div>
+        </div>
+      </section> 
+    </>
+  )
+}
+
+export default HomeIntro
